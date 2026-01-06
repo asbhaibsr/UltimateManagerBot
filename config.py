714572e305f4ffe119f7e82e0ec8dfeacd5a2499
Asbhaibsr <ffloverisarsad@gmail.com>
2026-01-06 21:44:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     # Bot Configuration
@@ -7,16 +10,16 @@ class Config:
     API_HASH = os.getenv("API_HASH", "your_api_hash_here")
     
     # Bot Info
-    BOT_USERNAME = "MovieMasterProBot"
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "MovieMasterProBot")
     OWNER_ID = int(os.getenv("OWNER_ID", 1234567890))
-    OWNER_USERNAME = "asbhai_bsr"
+    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "asbhai_bsr")
     
     # Database
     MONGO_DB_URL = os.getenv("MONGO_DB_URL", "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority")
     
     # Channels
-    MAIN_CHANNEL = "MovieProChannel"
-    LOG_CHANNEL = -1001234567890
+    MAIN_CHANNEL = os.getenv("MAIN_CHANNEL", "MovieProChannel")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1001234567890))
     
     # Port for Koyeb/Heroku
     PORT = int(os.getenv("PORT", 8080))
@@ -44,3 +47,4 @@ class Config:
     
     DEFAULT_AUTO_DELETE_TIME = 60  # 1 minute
     REQUEST_COOLDOWN = 300  # 5 minutes between requests
+    MAX_SEARCH_RESULTS = 10  # Maximum search results to show
