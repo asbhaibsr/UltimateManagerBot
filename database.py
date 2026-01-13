34f591e1d60756c3c82a124c5be4ade556fcbb17
Asbhaibsr <ffloverisarsad@gmail.com>
2026-01-13 19:52:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+import motor.motor_asyncio
 from config import Config
 
-client = AsyncIOMotorClient(Config.MONGO_DB_URL)
-db = client["movie_bot"]
+# MongoDB connection
+client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_DB_URL)
+db = client["movie_helper_bot"]
 
 # Collections
 users_col = db["users"]
@@ -10,15 +11,33 @@ groups_col = db["groups"]
 settings_col = db["settings"]
 force_sub_col = db["force_sub"]
 
-async def add_user(user_id, username=None, first_name=None):
+# User Functions
+async def add_user(user_id, username=None, first_name=None, last_name=None):
     await users_col.update_one(
         {"_id": user_id},
-        {"$set": {"username": username, "first_name": first_name, "banned": False}},
+        {
+            "$set": {
+                "username": username,
+                "first_name": first_name,
+                "last_name": last_name,
+                "banned": False,
+                "joined_at": datetime.datetime.now()
+            }
+        },
         upsert=True
     )
 
 async def get_user(user_id):
     return await users_col.find_one({"_id": user_id})
+
+async def get_all_users():
+    users = []
+    async for user in users_col.find({"banned": False}):
+        users.append(user["_id"])
+    return users
+
+async def get_user_count():
+    return await users_col.count_documents({"banned": False})
 
 async def ban_user(user_id):
     await users_col.update_one({"_id": user_id}, {"$set": {"banned": True}})
@@ -26,33 +45,36 @@ async def ban_user(user_id):
 async def unban_user(user_id):
     await users_col.update_one({"_id": user_id}, {"$set": {"banned": False}})
 
+# Group Functions
 async def add_group(group_id, title=None, username=None):
     await groups_col.update_one(
         {"_id": group_id},
-        {"$set": {"title": title, "username": username}},
+        {
+            "$set": {
+                "title": title,
+                "username": username,
+                "added_at": datetime.datetime.now()
+            }
+        },
         upsert=True
     )
-
-async def remove_group(group_id):
-    await groups_col.delete_one({"_id": group_id})
 
 async def get_group(group_id):
     return await groups_col.find_one({"_id": group_id})
 
-async def get_all_users():
-    cursor = users_col.find({"banned": False})
-    return [doc["_id"] async for doc in cursor]
-
 async def get_all_groups():
-    cursor = groups_col.find({})
-    return [doc["_id"] async for doc in cursor]
-
-async def get_user_count():
-    return await users_col.count_documents({"banned": False})
+    groups = []
+    async for group in groups_col.find({}):
+        groups.append(group["_id"])
+    return groups
 
 async def get_group_count():
     return await groups_col.count_documents({})
 
+async def remove_group(group_id):
+    await groups_col.delete_one({"_id": group_id})
+
+# Settings Functions
 async def get_settings(chat_id):
     settings = await settings_col.find_one({"_id": chat_id})
     if not settings:
@@ -61,7 +83,8 @@ async def get_settings(chat_id):
             "spelling_on": True,
             "auto_delete_on": False,
             "delete_time": 0,
-            "welcome_enabled": True
+            "welcome_enabled": True,
+            "force_sub_enabled": False
         }
         await settings_col.insert_one(default_settings)
         return default_settings
@@ -74,6 +97,7 @@ async def update_settings(chat_id, key, value):
         upsert=True
     )
 
+# Force Subscribe Functions
 async def set_force_sub(chat_id, channel_id):
     await force_sub_col.update_one(
         {"_id": chat_id},
@@ -87,9 +111,9 @@ async def get_force_sub(chat_id):
 async def remove_force_sub(chat_id):
     await force_sub_col.delete_one({"_id": chat_id})
 
+# Utility Functions
 async def clear_junk():
-    # Remove users who have blocked the bot (simulated by banned flag)
-    await users_col.delete_many({"banned": True})
-    # Remove groups where bot is no longer member (this would need external checking)
-    # For now, just return count
-    return await users_col.count_documents({"banned": True})
+    result = await users_col.delete_many({"banned": True})
+    return result.deleted_count
+
+import datetime
